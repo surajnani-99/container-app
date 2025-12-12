79cbed9041d0415ecf1dbaba4d048357ec612c33
surajnani-99 <vsurajkumar2003@gmail.com>
2025-12-12 06:15:49 +0000

    added swagger code

---
+++ java-microservice/src/main/java/com/example/demo/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Demo Java Microservice API")
+                        .description("Swagger documentation for Container App demo")
+                        .version("1.0.0"));
+    }
+}
+
